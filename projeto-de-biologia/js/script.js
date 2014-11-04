e4c0d4217fe9ba53270b2116889ed809354f6135
jotamota97 <jotamota97@users.noreply.github.com>
2014-11-04 17:30:14 +0000

    Refactoring

--- projeto-de-biologia/js/script.js
+++ projeto-de-biologia/js/script.js
@@ -15,68 +15,59 @@
 
   var Orgao = function ( id , title , description ) {
 
-    var self = this;
-
     if ( !id || !title || !description ) {
-      return self;
+      return this;
     }
 
     this.title = title;
     this.description = description;
+    this.$el = $( "polygon[title=" + id + "]" );
 
-    if ( $.isArray( id ) ) {
-
-      $.each( id , function(){
-
-        orgaos[ this ] = self;
-
-      } );
-
-    } else {
-
-      orgaos[ id ] = this;
-
-    }
+    orgaos[ id ] = this;
 
   };
 
-  /* Masculino */
+  var build = function(){
 
-  new Orgao( "testiculos" , "Testículos" ,
-    "Os testículos são os responsáveis pela produção de testosterona (hormona sexual). São nos testículos que ocorre a gametogénese, que no caso dos testículos tem o nome de espermatogénese. Exteriormente os testículos são cobertos pelo escroto. A sua temperatura é um pouco abaixo da temperatura normal do corpo, o que é essencial para a produção de espermatozóides viáveis; uma variação, mesmo que pequena, pode ser prejudicial."
-  );
+    /* Masculino */
 
-  new Orgao( "prostata" , "Próstata" ,
-    "A próstata é uma glândula exócrina que faz parte do sistema reprodutor masculino. A  sua função é produzir e armazenar um fluido incolor e ligeiramente alcalino (pH 7.29) que constitui 10-30% do volume do fluido seminal, que juntamente com os espermatozóides constitui o sémen."
-  );
+    new Orgao( "testiculos" , "Testículos" ,
+      "Os testículos são os responsáveis pela produção de testosterona (hormona sexual). São nos testículos que ocorre a gametogénese, que no caso dos testículos tem o nome de espermatogénese. Exteriormente os testículos são cobertos pelo escroto. A sua temperatura é um pouco abaixo da temperatura normal do corpo, o que é essencial para a produção de espermatozóides viáveis; uma variação, mesmo que pequena, pode ser prejudicial."
+    );
 
-  new Orgao( "glande" , "Glande" ,
-    "A glande é a parte sensível do órgão sexual masculino, situada na extremidade do pénis, que termina com a abertura da uretra. É uma expansão do corpo esponjoso presente no interior do pénis, ligada ao restante do órgão pelo frénulo e pela coroa. É recoberto pelo prepúcio em indivíduos não circuncidados."
-  );
+    new Orgao( "prostata" , "Próstata" ,
+      "A próstata é uma glândula exócrina que faz parte do sistema reprodutor masculino. A  sua função é produzir e armazenar um fluido incolor e ligeiramente alcalino (pH 7.29) que constitui 10-30% do volume do fluido seminal, que juntamente com os espermatozóides constitui o sémen."
+    );
 
-  new Orgao( "vesicula-seminar" , "Vesículas Seminais" ,
-    "As vesículas seminais são glândulas que juntamente com a próstata produzem as secreções que envolvem os espermatozóides, constituindo o esperma ou sémen."
-  );
+    new Orgao( "glande" , "Glande" ,
+      "A glande é a parte sensível do órgão sexual masculino, situada na extremidade do pénis, que termina com a abertura da uretra. É uma expansão do corpo esponjoso presente no interior do pénis, ligada ao restante do órgão pelo frénulo e pela coroa. É recoberto pelo prepúcio em indivíduos não circuncidados."
+    );
 
-  new Orgao( [ "canal-deferente-1" , "canal-deferente-2" ] , "Canais deferentes" ,
-    "Os canais deferentes são os locais onde os espermatozóides são armazenados e posteriormente transportados até à uretra."
-  );
+    new Orgao( "vesicula-seminar" , "Vesículas Seminais" ,
+      "As vesículas seminais são glândulas que juntamente com a próstata produzem as secreções que envolvem os espermatozóides, constituindo o esperma ou sémen."
+    );
 
-  new Orgao( "corpo-esponjoso" , "Corpo Esponjoso" ,
-    "O corpo esponjoso é um tecido, situado na parte inferior do pénis, que envolve e protege a uretra."
-  );
+    new Orgao( "canal-deferente" , "Canais deferentes" ,
+      "Os canais deferentes são os locais onde os espermatozóides são armazenados e posteriormente transportados até à uretra."
+    );
 
-  new Orgao( "epididimo" , "Epidídimo" ,
-    "Os epidídimos são os locais onde os espermatozóides são armazenados e onde sofrem a sua maturação."
-  );
+    new Orgao( "corpo-esponjoso" , "Corpo Esponjoso" ,
+      "O corpo esponjoso é um tecido, situado na parte inferior do pénis, que envolve e protege a uretra."
+    );
 
-  new Orgao( "corpo-cavernoso" , "Corpo Cavernoso" ,
-    "Os dois corpos cavernosos situam-se um ao lado do outro na parte superior do pénis. São um par de estruturas de tecido erétil parecidas com esponjas, que contêm a maior parte do sangue do pénis durante a ereção."
-  );
+    new Orgao( "epididimo" , "Epidídimo" ,
+      "Os epidídimos são os locais onde os espermatozóides são armazenados e onde sofrem a sua maturação."
+    );
 
-  /* Feminino */
+    new Orgao( "corpo-cavernoso" , "Corpo Cavernoso" ,
+      "Os dois corpos cavernosos situam-se um ao lado do outro na parte superior do pénis. São um par de estruturas de tecido erétil parecidas com esponjas, que contêm a maior parte do sangue do pénis durante a ereção."
+    );
 
-  new Orgao();
+    /* Feminino */
+
+    new Orgao();
+
+  };
 
   /* Setup */
 
@@ -106,39 +97,34 @@
 
       $infoDesc = $info.find( ".description" ),
 
-      $svg = $( "svg" ),
+      $svg = $( "svg" );
 
-      regexp = /canal-deferente/,
-
-      $canaisDeferentes = $( "#canal-deferente-1, #canal-deferente-2" );
+      // Build
+      build();
 
       $svg.find( "polygon" ).mouseenter( function ( e ) {
 
-        if ( this.id.match( regexp ) ) {
+        var thisOrgao = orgaos[ this.getAttribute( "title" ) ];
 
-          $canaisDeferentes.attr( "class" , "see" );
+        if ( thisOrgao ) {
 
-        } else {
-
-          $( this ).attr( "class" , "see" );
+          thisOrgao.$el.attr( "class" , "see" );
 
         }
 
       } ).mouseleave( function ( e ) {
 
-        if ( this.id.match( regexp ) ) {
+        var thisOrgao = orgaos[ this.getAttribute( "title" ) ];
 
-          $canaisDeferentes.attr( "class" , null );
+        if ( thisOrgao ) {
 
-        } else {
-
-          $( this ).attr( "class" , null );
+          thisOrgao.$el.attr( "class" , null );
 
         }
 
       } ).click( function ( e ) {
 
-        var thisOrgao = orgaos[ this.id ];
+        var thisOrgao = orgaos[ this.getAttribute( "title" ) ];
 
         if ( thisOrgao ) {
 
